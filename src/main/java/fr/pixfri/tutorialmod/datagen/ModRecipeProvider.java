7caa7415c28ac33b957c39fdbb78ff0e44f7e17d
Jean <jean.letessier@protonmail.com>
2024-05-15 17:55:30 +0200

    Implemented datagen.

---
+++ src/main/java/fr/pixfri/tutorialmod/datagen/ModRecipeProvider.java
@@ -0,0 +1,66 @@
+package fr.pixfri.tutorialmod.datagen;
+
+import fr.pixfri.tutorialmod.block.ModBlocks;
+import fr.pixfri.tutorialmod.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.block.Blocks;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.util.Identifier;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    private static final List<ItemConvertible> RUBY_SMELTABLES = List.of(ModItems.RAW_RUBY,
+            ModBlocks.RUBY_ORE, ModBlocks.DEEPSLATE_RUBY_ORE, ModBlocks.NETHER_RUBY_ORE, ModBlocks.END_STONE_RUBY_ORE);
+    private static final List<ItemConvertible> PLANKS = List.of(Blocks.ACACIA_PLANKS, Blocks.BAMBOO_PLANKS,
+            Blocks.BIRCH_PLANKS, Blocks.CHERRY_PLANKS, Blocks.CRIMSON_PLANKS, Blocks.DARK_OAK_PLANKS, Blocks.JUNGLE_PLANKS,
+            Blocks.OAK_PLANKS, Blocks.SPRUCE_PLANKS, Blocks.WARPED_PLANKS);
+
+    public ModRecipeProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generate(Consumer<RecipeJsonProvider> exporter) {
+        offerSmelting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
+                0.7f, 200, "ruby");
+        offerBlasting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
+                0.7f, 100, "ruby");
+
+        offerSmelting(exporter, PLANKS, RecipeCategory.MISC, ModItems.COAL_BRIQUETTE,
+                0.2f, 100, "coal_briquette");
+
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, ModItems.RUBY,
+                RecipeCategory.DECORATIONS, ModBlocks.RUBY_BLOCK);
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, ModItems.RAW_RUBY,
+                RecipeCategory.DECORATIONS, ModBlocks.RAW_RUBY_BLOCK);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.METAL_DETECTOR, 1)
+                .pattern("  S")
+                .pattern(" I ")
+                .pattern("IPI")
+                .input('S', Items.STICK)
+                .input('I', Items.IRON_INGOT)
+                .input('P', Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE)
+                .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE), conditionsFromItem(Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.METAL_DETECTOR)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.SOUND_BLOCK, 1)
+                .pattern("WWW")
+                .pattern("WRW")
+                .pattern("WWW")
+                .input('W', ItemTags.PLANKS)
+                .input('R', ModItems.RUBY)
+                .criterion(hasItem(ModItems.RUBY), conditionsFromItem(ModItems.RUBY))
+                .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.SOUND_BLOCK)));
+    }
+}
