ed2a2aa39ac88b26c5a01678bfd28d5b32573182
Jean <jean.letessier@protonmail.com>
2024-05-18 16:21:35 +0200

    Removed unused import.

--- src/main/java/fr/pixfri/tutorialmod/datagen/ModLootTableProvider.java
+++ src/main/java/fr/pixfri/tutorialmod/datagen/ModLootTableProvider.java
@@ -11,7 +11,6 @@ import net.minecraft.item.Item;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.entry.LeafEntry;
-import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.ApplyBonusLootFunction;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
