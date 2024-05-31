beaec588cd0d1ca0e4a21eca1cfd2ffcd884c1ba
Jean <jean.letessier@protonmail.com>
2024-05-31 22:43:43 +0200

    Added drops to suspicious sand loot table.

--- src/main/java/fr/pixfri/tutorialmod/loottable/ModLootTableModifiers.java
+++ src/main/java/fr/pixfri/tutorialmod/loottable/ModLootTableModifiers.java
@@ -3,18 +3,27 @@ package fr.pixfri.tutorialmod.loottable;
 import fr.pixfri.tutorialmod.item.ModItems;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ModLootTableModifiers {
     public static final Identifier JUNGLE_TEMPLE_ID =
             new Identifier("minecraft", "chests/jungle_temple");
     public static final Identifier CREEPER_ID =
             new Identifier("minecraft", "entities/creeper");
+
+    public static final Identifier SUSPICIOUS_SAND_ID =
+            new Identifier("minecraft", "archaeology/desert_pyramid");
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
@@ -37,6 +46,19 @@ public class ModLootTableModifiers {
 
                 tableBuilder.pool(poolBuilder.build());
             }
+        });
+
+        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) -> {
+            if(id.equals(SUSPICIOUS_SAND_ID)) {
+                List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
+                entries.add(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
+                entries.add(ItemEntry.builder(ModItems.COAL_BRIQUETTE).build());
+
+                LootPool.Builder pool = LootPool.builder().with(entries);
+                return LootTable.builder().pool(pool).build();
+            }
+
+            return null;
         });
     }
 }
